842174f036c53d36ecf3b5814bfa75ebb508fd52
Matthew Tretter <matthew@exanimo.com>
2011-05-06 12:36:27 -0400

    You can now have the maid watch all of a model's file fields without listing them out.

--- maid/base.py
+++ maid/base.py
@@ -1,17 +1,18 @@
 from django.db.models.signals import post_delete, pre_save, post_save
 import weakref
+from collections import defaultdict
 
 
 class FileFieldRegistry(object):
     
     # Maps models to a set of file field properties whose files should be
     # deleted when orphaned.
-    _registered_fields = {}
+    _registered_fields = defaultdict(set)
     
     # Maps model instances that are about to be saved to their old versions.
     _old_instances = weakref.WeakKeyDictionary()
     
-    def register(self, model, field_names):
+    def register(self, model, field_names=None):
         """
         Registers a FileField with the maid. When a new file is saved or the
         current value is deleted, she'll get rid of the old file.
@@ -23,9 +24,13 @@ class FileFieldRegistry(object):
             post_delete.connect(self._model_post_delete, sender=model)
             pre_save.connect(self._model_pre_save, sender=model)
             post_save.connect(self._model_post_save, sender=model)
-            
+        
+        if field_names is None:
+            # Register all of the model's fields.
+            field_names = set([field.attname for field in model._meta.fields])
+
         # Register the new fields.
-        self._registered_fields[model] = self._registered_fields.get(model, set()) | set(field_names)
+        self._registered_fields[model] |= set(field_names)
 
     def _model_pre_save(self, instance, sender, **kwargs):
         # We don't need to check for changed file fields if this is a new
@@ -58,26 +63,32 @@ class FileFieldRegistry(object):
         field = model_type._meta.get_field(field_name)
         file = getattr(instance, field_name)
     
-        # If no other object of this type references the file, 
-        # and it's not the default value for future objects, 
-        # delete it from the backend.
-        if file and file.name != field.default and \
-            not model_type._default_manager.filter(**{field.name: file.name}).exclude(pk=instance.pk):
-                # Don't save the instance because that would trigger the
-                # pre_save signal again.
-                file.delete(save=False) 
-        elif file:
-            # Otherwise, just close the file, so it doesn't tie up resources. 
-            file.close()
+        try:
+            perform_deletion = file and file.name != field.default
+        except AttributeError:
+            # Wasn't actually a file.
+            pass
+        else:
+            # If no other object of this type references the file, 
+            # and it's not the default value for future objects, 
+            # delete it from the backend.
+            if perform_deletion and not \
+                model_type._default_manager.filter(**{field.name: file.name}).exclude(pk=instance.pk):
+                    # Don't save the instance because that would trigger the
+                    # pre_save signal again.
+                    file.delete(save=False) 
+            else:
+                # Otherwise, just close the file, so it doesn't tie up resources. 
+                file.close()
 
 
 # The registry instance.
 file_field_registry = FileFieldRegistry()
 
 
-def register_file_fields(model, *fields):
+def register_file_fields(model, field_names=None):
     """
     Register a FileField (or similar) to have its unused files automatically
     deleted.
     """
-    file_field_registry.register(model, fields)
+    file_field_registry.register(model, field_names)
--- maid/tests.py
+++ maid/tests.py
@@ -4,14 +4,16 @@ import os.path
 import maid
 from django.core.files.base import File
 from django.db import models
-import sys
 
 
 class TestModel(models.Model):
     document = models.FileField(upload_to='testmodel')
 
+class TestModel2(models.Model):
+    document = models.FileField(upload_to='testmodel')
 
-maid.register_file_fields(TestModel, 'document')
+maid.register_file_fields(TestModel, ['document'])
+maid.register_file_fields(TestModel2)
 
 
 def _create_file(filename):
@@ -21,26 +23,37 @@ def _create_file(filename):
     return file
 
 
-def _create_model_instance(filename):
-    return TestModel.objects.create(document=_create_file(filename))
+def _create_model_instance(model, filename):
+    return model.objects.create(document=_create_file(filename))
 
 
 class FileFieldCleanupTest(TestCase):
     
-    def test_delete_model(self):
-        model = _create_model_instance('file_1.txt')
+    def test_delete_model_1(self):
+        self._test_delete_model(TestModel)
+    
+    def test_delete_model_2(self):
+        self._test_delete_model(TestModel2)
+    
+    def test_save_new(self):
+        self._test_save_new(TestModel)
+    
+    def test_save_new_2(self):
+        self._test_save_new(TestModel2)
+    
+    def _test_delete_model(self, model):
+        model = _create_model_instance(model, 'file_1.txt')
         file_path = model.document.path
         model.delete()
         self.assertFileExists(file_path, False)
     
-    def test_save_new(self):
-        model = _create_model_instance('file_2.txt')
+    def _test_save_new(self, model):
+        model = _create_model_instance(model, 'file_2.txt')
         old_path = model.document.path
         model.document = _create_file('file_2.txt')
         model.save()
         self.assertFileExists(old_path, False)
         self.assertFileExists(model.document.path)
-
 
     def assertFileExists(self, file_path, assertion=True):
         # Verify that the file was deleted.
